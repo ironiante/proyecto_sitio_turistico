90e7ec83059cb7aacc1f185d688fb8ce8daa7de5
jirman rodriguez <jirman.rodriguez@uptc.edu.co>
2021-11-09 21:07:46 -0500

    proyecto base

---
+++ app/src/main/java/com/jirmanrodriguez/misiontic/proyecto_sitio_turistico/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jirmanrodriguez.misiontic.proyecto_sitio_turistico
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
